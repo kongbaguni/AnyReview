a1ac1d68d83583995583e13ceba7c72cc8e8d5a2
kongbaguni <kongbaguni@gmail.com>
2020-06-16 10:42:13 +0900

    Initial Commit

---
+++ AnyReview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnyReview
+//
+//  Created by Changyul Seo on 2020/06/16.
+//  Copyright © 2020 Changyul Seo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
